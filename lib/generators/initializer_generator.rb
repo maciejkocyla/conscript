d8ed16f92e66379cc0805702ec7becea7a9ce11e
Maciej Kocyła <maciej.kocyla@gmail.com>
2014-03-09 19:48:05 +0100

    user_model_autocomplete method

--- lib/generators/initializer_generator.rb
+++ lib/generators/initializer_generator.rb
@@ -1,4 +1,4 @@
-class InitializerGenerator < Rails::Generators::NamedBase
+class InitializerGenerator < Rails::Generators::Base
 
   def create_initializer_file
     create_file "config/initializers/initializer.rb", "# Add initialization content here"
@@ -48,6 +48,36 @@ class InitializerGenerator < Rails::Generators::NamedBase
 
     RUBY
   end
+  end
+
+  def users_model_autocomplete
+    inject_into_file "app/models/user.rb", after: "class User < ActiveRecord::Base\n" do <<-'RUBY'
+        
+      has_secure_password      
+      before_save { self.email = email.downcase } 
+      before_save :create_remember_token
+      validates :password, length: { minimum: 6 }
+      validates :name, presence: true, length: { maximum: 25 }
+      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+      def User.new_remember_token
+        SecureRandom.urlsafe_base64
+      end
+       
+      def User.encrypt(token)
+        Digest::SHA1.hexdigest(token.to_s)
+      end
+      
+      private
+        def create_remember_token
+          if self.remember_token.nil?     
+            self.remember_token = User.encrypt(User.new_remember_token)
+          end
+        end
+
+    RUBY
+    end
   end
 
 end
